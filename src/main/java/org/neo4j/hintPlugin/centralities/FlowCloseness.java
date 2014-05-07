eb5f15551403ef1d7059ce6d89c42ef4cdf6a380
franciscogutierrez <fsalvador23@gmail.com>
2014-05-07 12:19:23 -0500

    Fixed project scaffold

---
+++ src/main/java/org/neo4j/hintPlugin/centralities/FlowCloseness.java
@@ -0,0 +1,91 @@
+/**
+ * Maximum Flow Class
+ * Returns the Maximum flow value within a graph given a source and sink nodes.
+ * @author  Francisco Gutiérrez.
+ * @version 0.1
+ * @since 2014-05-01
+ */
+package org.neo4j.hintplugin.centralities;
+
+import java.nio.charset.Charset;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import static org.neo4j.graphalgo.GraphAlgoFactory.allPaths;
+import static org.neo4j.kernel.Traversal.expanderForAllTypes;
+import org.neo4j.graphdb.*;
+import org.neo4j.helpers.collection.IteratorUtil;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Path("/flowcloseness")
+public class FlowCloseness {
+
+    private final GraphDatabaseService database;
+    private Node nSource;
+    private Node nSink;
+
+    enum MyRelationshipTypes implements RelationshipType
+    {
+        KNOWS, IS_SIMILAR
+    }
+
+    public FlowCloseness( @Context GraphDatabaseService database ) {
+        this.database = database;
+    }
+    
+    @GET
+    @Produces( MediaType.TEXT_PLAIN )
+    @Path( "/{source}/{sink}" )
+    public Response maximumflow(@PathParam("source") long source, @PathParam("sink") long sink) {
+        String json = "{\"flowcloseness\":" + this.getFlow(source, sink) + "}";
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+    }
+    
+    /*
+     * Calculates the MaximumFlow given a source and sink nodes.
+     *
+     * @param source    the source node id
+     * @param sink      the sink node id
+     * @return double   as the maximum flow value.
+     */
+    
+    private double getFlow(long source, long sink){
+        int maxDepth = 10000;
+        int flow = 0;
+        int accumulator = 0;
+        
+        List<Integer> flows = new ArrayList<Integer>();
+        Transaction tx = database.beginTx();
+        try {
+            this.nSource = database.getNodeById(source);
+            this.nSink = database.getNodeById(sink);
+            
+            for(org.neo4j.graphdb.Path p : allPaths(expanderForAllTypes(Direction.BOTH),maxDepth).findAllPaths(this.nSource,this.nSink)) {
+                for (Relationship r : p.relationships()) {
+                    flows.add((Integer)r.getProperty("weight"));
+                }
+                flow = Collections.min(flows);
+                flows.clear();
+                accumulator = accumulator + flow;
+                for (Relationship r: p.relationships()){
+                    r.setProperty("weight",(Integer)r.getProperty("weight") - flow);
+                }
+            }
+            System.out.println("*********         Accumulator: " + accumulator);
+            tx.success();
+        } catch (Exception e) {
+            System.out.println("********* Fail, This happened: " + e);
+            tx.failure();
+        } finally {
+             tx.close();
+        }
+        return accumulator;
+    }
+}
---
+++ src/test/java/org/neo4j/mytests/SimilarityTest.java
@@ -0,0 +1,102 @@
+package org.neo4j.mytests;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mortbay.jetty.LocalConnector;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.server.CommunityNeoServer;
+import org.neo4j.server.helpers.CommunityServerBuilder;
+import org.neo4j.graphdb.RelationshipType;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SimilarityTest {
+    private GraphDatabaseAPI db;
+    private CommunityNeoServer server;
+    
+    enum MyRelationshipTypes implements RelationshipType
+    {
+        CONTAINED_IN, KNOWS
+    }
+    
+    
+    @Before
+    public void before() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(0);
+        server = CommunityServerBuilder
+        .server()
+        .onPort(serverSocket.getLocalPort())
+        .withThirdPartyJaxRsPackage("org.neo4j.hintplugin.utils", "/hintplugin/utils")
+        .build();
+        server.start();
+        db = server.getDatabase().getGraph();
+    }
+    
+    @After
+    public void after() {
+        server.stop();
+    }
+    /*
+    @Test
+    public void shouldReturnAllTheNodes() {
+        Transaction tx = db.beginTx();
+        Node a = db.createNode();
+        Node b = db.createNode();
+        Node c = db.createNode();
+        Node d = db.createNode();
+        Node e = db.createNode();
+        Node f = db.createNode();
+        a.setProperty("name", "Mark");
+        b.setProperty("name", "Travis");
+        c.setProperty("name", "Tom");
+        d.setProperty("name", "Scott");
+        e.setProperty("name", "Kevin");
+        f.setProperty("name", "Matt");
+        
+        a.createRelationshipTo(c, MyRelationshipTypes.KNOWS);
+        a.createRelationshipTo(d, MyRelationshipTypes.KNOWS);
+        a.createRelationshipTo(e, MyRelationshipTypes.KNOWS);
+        b.createRelationshipTo(e, MyRelationshipTypes.KNOWS);
+        b.createRelationshipTo(d, MyRelationshipTypes.KNOWS);
+        b.createRelationshipTo(f, MyRelationshipTypes.KNOWS);
+        
+        tx.success();
+        tx.close();
+        int statusCode = 0;
+        String node_a = "0";
+        String node_b = "1";
+        String address = server.baseUri().toString()+
+                        "hintplugin/utils/similarity/"+
+                        node_a + "/" + node_b;
+        try{
+            URL url = new URL(address);
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            statusCode = http.getResponseCode();
+            System.out.println("Connection RESPONSE CODE: " + statusCode);
+        }
+            catch(IOException ex){
+        }
+        assertEquals("200",statusCode + "");
+    }
+    */
+    private Client jerseyClient() {
+        DefaultClientConfig defaultClientConfig = new DefaultClientConfig();
+        defaultClientConfig.getClasses().add(JacksonJsonProvider.class);
+        return Client.create(defaultClientConfig);
+    }
+}
